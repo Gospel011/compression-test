eec6fa800f3291dd0be14542759be983218d850b
Gospel <gospel@Gospels-MacBook-Pro.local>
2025-08-27 21:11:13 +0100

    Made entire image preview page scale with pinch

--- lib/full_image_preview.dart
+++ lib/full_image_preview.dart
@@ -3,41 +3,64 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_image_compress/flutter_image_compress.dart';
 
-class FullImagePreview extends StatelessWidget {
+class FullImagePreview extends StatefulWidget {
   const FullImagePreview({super.key, required this.image});
   final XFile image;
 
   @override
+  State<FullImagePreview> createState() => _FullImagePreviewState();
+}
+
+class _FullImagePreviewState extends State<FullImagePreview> {
+  late TransformationController transformationController;
+  @override
+  void initState() {
+    super.initState();
+    transformationController = TransformationController();
+  }
+
+  @override
+  void dispose() {
+    transformationController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Stack(
-          children: [
-            Center(
-              child: InteractiveViewer(
-                child: Image.file(
-                  File(image.path),
-                  fit: BoxFit.cover,
+      body: InteractiveViewer(
+        transformationController: transformationController,
+        child: GestureDetector(
+          onDoubleTap: () {
+              transformationController.value = Matrix4.identity();
+            // setState(() {
+            // });
+          },
+          child: SafeArea(
+            child: Stack(
+              children: [
+                Center(
+                  child: Image.file(File(widget.image.path), fit: BoxFit.cover),
                 ),
-              ),
-            ),
 
-            Positioned(
-              top: 16,
-              right: 16,
-              child: Container(
-                // padding: EdgeInsets.all(2),
-                decoration: BoxDecoration(
-                  color: Colors.deepPurpleAccent,
-                  shape: BoxShape.circle,
+                Positioned(
+                  top: 16,
+                  right: 16,
+                  child: Container(
+                    // padding: EdgeInsets.all(2),
+                    decoration: BoxDecoration(
+                      color: Colors.deepPurpleAccent,
+                      shape: BoxShape.circle,
+                    ),
+                    child: IconButton(
+                      onPressed: () => Navigator.pop(context),
+                      icon: Icon(Icons.close, color: Colors.white),
+                    ),
+                  ),
                 ),
-                child: IconButton(
-                  onPressed: () => Navigator.pop(context),
-                  icon: Icon(Icons.close, color: Colors.white),
-                ),
-              ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
